1be4860128e8d5ac9c7c482922a3b1a846847b8b
AS1038054 <adhiraj.singh@mindtree.com>
2017-04-27 15:16:57 -0400

    Class
    Class update

--- src/test/java/com/MT/rough/TestProperties.java
+++ src/test/java/com/MT/rough/TestProperties.java
@@ -1,11 +1,23 @@
 package com.MT.rough;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Properties;
 
 /**
  * Created by adhirajsingh on 4/26/17.
  */
 public class TestProperties {
+    public static void main(String args[]) throws IOException {
+
         Properties config = new Properties();
-        
+        System.out.println(System.getProperty("user.dir"));
+        FileInputStream fio = new FileInputStream(System.getProperty("user.dir")+"/src/main/resources/properties/config.properties");
+        config.load(fio);
+        System.out.println(config.getProperty("browser"));
+    }
+    public TestProperties() throws FileNotFoundException {
+        }
+
 }
